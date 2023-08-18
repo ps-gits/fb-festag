102e1cca6f138230b3d03be21facca2410005682
PRATIK SHARMA <89690292+ps-gits@users.noreply.github.com>
2023-08-18 12:34:24 +0530

    Delete temnext-config-plugins.ts

--- src/temnext-config-plugins.ts
+++
@@ -1,4 +0,0 @@
-exports.graphqlPlugin = require('../lib/next-config/plugins/graphql');
-exports.robotsPlugin = require('../lib/next-config/plugins/robots');
-exports.sassPlugin = require('../lib/next-config/plugins/sass');
-exports.sitemapPlugin = require('../lib/next-config/plugins/sitemap');
