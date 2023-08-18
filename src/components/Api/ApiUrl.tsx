cba023d5e966183ab81b1540c9cf0c7cf40f9675
PRATIK SHARMA <89690292+ps-gits@users.noreply.github.com>
2023-08-18 14:13:09 +0530

    Update ApiUrl.tsx

--- src/components/Api/ApiUrl.tsx
+++ src/components/Api/ApiUrl.tsx
@@ -1,4 +1,6 @@
 // export const url = 'http://20.233.74.9:3000/'
-export const url = 'https://flight.manageprojects.in/';
+// export const url = 'https://flight.manageprojects.in/';
+
+export const url = 'https://apis.flybeond.com';
 export const graphQLUrl = 'https://edge.sitecorecloud.io/api/graphql/v1';
 
