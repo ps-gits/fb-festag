2143bef71773f8adacfccc6d6b409930587ff358
Pratik Sharma <contact@drafteq.com>
2023-08-27 01:47:55 +0530

    updated multiple files

--- src/components/Api/ApiUrl.tsx
+++ src/components/Api/ApiUrl.tsx
@@ -3,3 +3,5 @@
 
 export const url = 'https://apis.flybeond.com/';
 export const graphQLUrl = 'https://edge.sitecorecloud.io/api/graphql/v1';
+export const graphQLToken =
+  'ampjVGM4ODNnRTZEa0RjSFNrckx0ZXpQenJEZHlHYUFqbmJwQUh0RlNWVT18YXJhYmVzcXVlZmwwZjcwLWRlbW9wcm9qZWN0LWRlbW9lbnYtNzliYw==';
